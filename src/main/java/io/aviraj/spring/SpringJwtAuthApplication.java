387bf01ef6612cb4560cc3c1a9e2e4be98badf2c
Aviraj <Aviraj@DESKTOP-TVPIGGE>
2020-05-31 04:24:44 +0530

    First Draft

---
+++ src/main/java/io/aviraj/spring/SpringJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package io.aviraj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAuthApplication.class, args);
+	}
+
+}
